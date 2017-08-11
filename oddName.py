4a079c427b9bae4001b48eab6ee09bdfab63dfda
JTLANDER97 <jordan.holmes@my.jcu.edu.au>
2017-08-11 22:41:06 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,14 @@
 """Jordan Holmes"""
+
+valid_user_name = False
+user_name = str
+
+while valid_user_name is not True:
+    user_name = str(input("What is your name?: "))
+    if user_name == "":
+        print("Invalid entry!")
+    else:
+        valid_user_name = True
+
+for char in range(0, (len(user_name) + 1), 2):
+    print(user_name[char])
